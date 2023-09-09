694e80d73f282c66fd5ad8fed97b0a6913e5ca7f
Karanvir Sandhu <k2sandhu@ucsd.edu>
2023-09-08 20:02:32 -0700

    Working on createPrompt and createQuestions using ec6

--- src/lib.js
+++ src/lib.js
@@ -2,14 +2,27 @@ import fs from 'fs'
 
 export const chooseRandom = () => {
   // TODO implement chooseRandom
+  //takes an array and an integer and creates a new array of size integer
+  // and returns a randomly populated array of original array elements
+
+
+  const shuffled = (array,numItems) =>[...array].sort(() => 0.5 - Math.random());
+
+  return shuffled.slice(0, numItems);
+
+  //const getRandomItem = (array,numItems) => array.get([...array.keys()][Math.floor(Math.random() * numItems)])
+  //const getRandomItem = (array,numItems) => [...array][Math.floor(Math.random()*numItems)]
 }
 
 export const createPrompt = () => {
-  // TODO implement createPrompt
+// create obj with 2 properties numQuestions and numChoises
+//returns array with specific objects
+//Wrong direction have to go back and restart
 }
 
 export const createQuestions = () => {
-  // TODO implement createQuestions
+  //Going in the wrong direction have to go back and restart
+
 }
 
 export const readFile = path =>
