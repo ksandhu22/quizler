1e00f83d60f44c8c6a220327be3086ef5f200273
Karanvir Sandhu <k2sandhu@ucsd.edu>
2023-09-10 02:33:00 -0700

    Completed and tested createPrompt

--- src/lib.js
+++ src/lib.js
@@ -7,22 +7,40 @@ export const chooseRandom = (array, numItems) => {
   if(array.length == 0 || array.length == 1){
     return array;
   }
-
   if(numItems < 1 || numItems > array.length){
     numItems = Math.random() * (array.length - 1) + 1;
   }
-
-
    return [...array].sort(() => 0.5 - Math.random()).slice(0, numItems);
-  
-  //const getRandomItem = (array,numItems) => array.get([...array.keys()][Math.floor(Math.random() * numItems)])
-  //const getRandomItem = (array,numItems) => [...array][Math.floor(Math.random()*numItems)]
+
+
 }
 
-export const createPrompt = () => {
-// create obj with 2 properties numQuestions and numChoises
-//returns array with specific objects
-//Wrong direction have to go back and restart
+export const createPrompt = ({ numQuestions = 1, numChoices = 2 } = {}) => {
+  const questions = questionNumber => ({
+    type: 'input',
+    name: `question-${questionNumber}`,
+    message: `Enter question ${questionNumber}`
+  })
+
+  const choices = (choiceNumber, questionNumber) => ({
+    type: 'input',
+    name: `question-${questionNumber}-choice-${choiceNumber}`,
+    message: `Enter answer choice ${choiceNumber} for question ${questionNumber}`
+  })
+
+  return Array(numQuestions + numQuestions * numChoices)
+  .fill()
+  .map((value, index) => {
+    if (index % (numChoices + 1) === 0) {
+      return questions(index / (numChoices + 1) ? index / (numChoices + 1) : 1)
+    } 
+    
+    else {
+      return choices(index % (numChoices + 1),
+        Math.ceil(index / (numChoices + 1))
+      )
+    }
+  })
 }
 
 export const createQuestions = () => {
